9efc7f32db87196d99906427dcde34393dd71807
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-07 12:01:49 +0530

    jan work

--- 7jan/index.js
+++ 7jan/index.js
@@ -1,1 +1,5 @@
 console.log("Hello, World! This is the 7jan directory.");
+console.log("This file serves as an entry point for the 7jan directory.");
+console.log("Feel free to add more functionality here!");
+console.log("Goodbye from 7jan!");
+
