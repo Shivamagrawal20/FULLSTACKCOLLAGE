1c250240d992d4c0b08da9f51dfa6befeb61814e
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-08 10:44:42 +0530

    Removed existing files

--- 7jan/index.js
+++
@@ -1,5 +0,0 @@
-console.log("Hello, World! This is the 7jan directory.");
-console.log("This file serves as an entry point for the 7jan directory.");
-console.log("Feel free to add more functionality here!");
-console.log("Goodbye from 7jan!");
-
