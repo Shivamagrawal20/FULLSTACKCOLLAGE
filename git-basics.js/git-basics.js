460d66cd3851d7c4d035067a29091ac50a0e284d
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-08 10:51:35 +0530

    basics of git

---
+++ git-basics.js/git-basics.js
@@ -0,0 +1,3 @@
+console.log("Welcome to Git Basics!");
+console.log("This is a simple JavaScript file to demonstrate Git basics.");
+console.log("You can modify this file and track changes using Git.");
