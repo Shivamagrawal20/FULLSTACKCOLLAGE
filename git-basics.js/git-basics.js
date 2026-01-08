ef5fe967632fac8e9af9487ef0fcfc92e93c928d
Shivam Agrawal <shivamprfs@gmail.com>
2026-01-08 11:09:33 +0530

    Basic Commands

--- git-basics.js/git-basics.js
+++ git-basics.js/git-basics.js
@@ -1,3 +1,35 @@
 console.log("Welcome to Git Basics!");
 console.log("This is a simple JavaScript file to demonstrate Git basics.");
 console.log("You can modify this file and track changes using Git.");
+
+/*
+Installing Git
+brew install git
+git --version 
+*/
+
+/*
+git config --global user.name "Your Name"
+git config --global user.email "your.email@example.com"
+*/
+
+/*
+git clone https://github.com/username/repository.git
+git clone https://github.com/username/repository.git new-folder-name
+*/
+
+/*
+git status
+git add .
+git commit -m "Your commit message"
+git push origin main
+*/
+
+/*
+git pull origin main
+
+*/
+
+
+
+
